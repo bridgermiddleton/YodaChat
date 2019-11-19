f6e939593f5c14d64dd7a9a9791e997fb5204020
Bridger Middleton <bmiddy3@gmail.com>
2019-11-19 15:00:37 -0700

    signal r still working. translation incorporated.

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -6,14 +6,29 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
+    console.log(message);
+    const encodedMessage = encodeURIComponent(message);
+    fetch(`https://yodish.p.rapidapi.com/yoda.json?text=${encodedMessage}`, {
+        "method": "POST",
+        "headers": {
+            "x-rapidapi-host": "yodish.p.rapidapi.com",
+            "x-rapidapi-key": "e9077e6ee9mshe29568a25c117e8p111788jsn2ad418d390bc",
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "body": {}
+    }).then(response => response.json()).then(data => {
+        console.log(data.contents.translated);
+        var li = document.createElement("li");
+        var encodedMsg = user + " says " + data.contents.translated;
+        li.textContent = encodedMsg;
+        document.getElementById("messagesList").appendChild(li);
+
+    });
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+
 });
 
-connection.start().then(function(){
+connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
